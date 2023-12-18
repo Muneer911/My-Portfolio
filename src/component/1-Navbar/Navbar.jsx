395e845c9309db4bf6899fb6cd2e92a06b84e2c1
Muneer <ahmed.elsadig.ahmed@gmail.com>
2023-12-18 02:26:05 +0200

    feat: Navbar buttons active

--- src/component/1-Navbar/Navbar.jsx
+++ src/component/1-Navbar/Navbar.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import './NavbarStyle.scss'
-import SideBar from '../sideBar/sideBar'
-
+import React from "react";
+import "./NavbarStyle.scss";
+import SideBar from "../sideBar/sideBar";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      
-    <SideBar/>
+    <div className="navbar">
+      <SideBar />
       <div className="wrapper nav__padding">
-        
         <span>RAMEZ</span>
 
         <div className="social">
-          <a href="#">
-             <img src="/GitHub.png" alt="f-logo"/>
+          <a href="https://github.com/Ramez991">
+            <img src="/GitHub.png" alt="f-logo" />
+          </a>
+          <a href="https://twitter.com/AeMD911">
+            <img src="/X.png" alt="X-logo" />
           </a>
           <a href="#">
-             <img src="/X.png" alt="X"/>
-          </a>
-          <a href="#">
-             <img src="/Linkedin.png" alt="Linkedin"/>
+            <img src="/Linkedin.png" alt="Linkedin-logo" />
           </a>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
