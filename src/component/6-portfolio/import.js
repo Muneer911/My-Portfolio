54d442157c6a4cb817bda2f53f5811711366ac30
Muneer <ahmed.elsadig.ahmed@gmail.com>
2023-12-29 14:53:29 +0200

    add: new js-import file

---
+++ src/component/6-portfolio/import.js
@@ -0,0 +1,9 @@
+import Griche from "../../../public/Wallpaper/Griche.png"
+import GPT3 from "../../../public/Wallpaper/GPT3.png"
+import MoiveX from "../../../public/Wallpaper/MovieX.png"
+
+export {
+    Griche,
+    GPT3,
+    MoiveX
+}
