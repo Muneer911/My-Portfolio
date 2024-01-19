3b2ce9229b69d359c7e319e5d14eeeee9f1c7519
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-19 14:38:30 +0200

    add: handleSuccess component

---
+++ src/component/5-contact/components/HandleSuccess.jsx
@@ -0,0 +1,9 @@
+export const HandleSuccess = () => {
+  return (
+    <div>
+      <p className="zzz" style={{ color: "white", fontSize: 15, margin: 10 }}>
+        THANK YOU{" "}
+      </p>
+    </div>
+  );
+};
