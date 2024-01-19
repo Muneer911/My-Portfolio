14f7c436d3df6c4bfe8db4cab2368bb880153172
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-19 14:38:02 +0200

    add: handleErrors component

---
+++ src/component/5-contact/components/HandleErrors.jsx
@@ -0,0 +1,9 @@
+export const HandleErrors = () => {
+  return (
+    <div>
+      <p className="zzz" style={{ color: "white", fontSize: 15, margin: 10 }}>
+        Sorry there's an error
+      </p>
+    </div>
+  );
+};
