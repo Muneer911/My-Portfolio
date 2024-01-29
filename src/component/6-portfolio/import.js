d203087de9332025e4607ed773f948a2d2a41624
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-29 16:54:25 +0200

    feat: add the weather to import

--- src/component/6-portfolio/import.js
+++ src/component/6-portfolio/import.js
@@ -1,9 +1,6 @@
-import Griche from "../../../public/Wallpaper/Griche.png"
-import GPT3 from "../../../public/Wallpaper/GPT3.png"
-import MoiveX from "../../../public/Wallpaper/MovieX.png"
+import Griche from "../../../public/Wallpaper/Griche.png";
+import GPT3 from "../../../public/Wallpaper/GPT3.png";
+import MoiveX from "../../../public/Wallpaper/MovieX.png";
+import Weatherz from "../../../public/Wallpaper/Weatherz.png";
 
-export {
-    Griche,
-    GPT3,
-    MoiveX
-}
+export { Griche, GPT3, MoiveX, Weatherz };
