af4483eb5c3b3b175d45c1bd8d3c3f67c7f20c88
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-29 16:54:56 +0200

    add: new weather app project

--- src/component/6-portfolio/Portfolio.jsx
+++ src/component/6-portfolio/Portfolio.jsx
@@ -1,16 +1,16 @@
 import "./PortfolioStyle.scss";
 import { useRef } from "react";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
-import { Griche, GPT3, MoiveX } from "./import";
+import { Griche, GPT3, MoiveX, Weatherz } from "./import";
 
 const items = [
   {
     id: 1,
-    title: "Gericht resturant",
-    img: Griche ,
+    title: "Gericht restaurant",
+    img: Griche,
     altg: "Griche restaurant image",
     desc: "Experience modern griche cuisine with our innovative menu and order from our high-skilled chefs.",
-    link: "https://gericht-restaurant-website-virid.vercel.app/"
+    link: "https://gericht-restaurant-website-virid.vercel.app/",
   },
   {
     id: 2,
@@ -18,15 +18,15 @@ const items = [
     img: GPT3,
     altg: "GPT-3 website image",
     desc: "Unleash the power of AI with our cutting-edge GPT-3 platform.",
-    link: "https://chat9.vercel.app/"
+    link: "https://chat9.vercel.app/",
   },
   {
     id: 3,
-    title: "Movie-x",
-    img: MoiveX,
-    altg: "Movie-X website image",
-    desc: "Dive into cinematic worlds: Explore the hottest new films & serie.",
-    link: "https://moviexpro.vercel.app/"
+    title: "WeatherAppz",
+    img: Weatherz,
+    altg: "WeatherApp website image",
+    desc: "Our weather app provides real-time updates on current weather conditions",
+    link: "https://myweather-app-nine.vercel.app/",
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const items = [
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     altg: "MV website image",
     desc: "Coming soon...",
-    link: ""
+    link: "",
   },
 ];
 
@@ -48,21 +48,19 @@ const Single = ({ item }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section >
+    <section>
       <div className="container">
         <div className="wrapper section__padding">
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt={item.altg} />
           </div>
-          <motion.div className="textContainer" style={{y}}>
+          <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            
-              <a href={item.link} target="_blank">
-            <button>
-              See Demo
-            </button>
-              </a>
+
+            <a href={item.link} target="_blank">
+              <button>See Demo</button>
+            </a>
           </motion.div>
         </div>
       </div>
